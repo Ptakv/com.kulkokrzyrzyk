b7e50c5e33cd3619a0dc13bd7d914f0efaf71178
Ptakv <ptakv1722@gmail.com>
2021-02-19 18:12:32 +0100

    kulkokrzyrzyk

--- src/Frame.java
+++ src/Frame.java
@@ -3,5 +3,14 @@ import javax.swing.JFrame;
 public class Frame extends JFrame
 {
     private static final long serialVersionUID = 1L;
-    
+    public Frame()
+    {
+        super("kolko i krzyzyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLocation(300, 100);
+
+
+        setVisible(true);
+    }
 }
