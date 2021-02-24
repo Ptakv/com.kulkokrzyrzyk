43ff889c82b525f56fb3f4e5f8b65d95a1b9ea46
Ptakv <ptakv1722@gmail.com>
2021-02-24 18:16:35 +0100

    qwer

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,62 +1,100 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-public class Buttons extends JPanel implements ActionListener 
+import java.awt.*;
+public class Buttons extends JPanel implements ActionListener
 {
     private static final long serialVersionUID = 1L;
     JButton reset;
     JButton[] button = new JButton[9];
-    boolean turn = true;
-    
-
-    public Buttons() 
+    boolean myTurn = true;
+    public Buttons()
     {
-
-        for (int i = 0; i < button.length; i++) 
-        {
-            button[i] = new JButton("");
-            button[i].addActionListener(this);
-            add(button[i]);
-        }
-
-        reset = new JButton("RESET");
-        add(reset);
-        reset.addActionListener(this);
-        setLayout(new GridLayout(4, 3));
+      for (int i = 0; i < button.length; i++)
+      {
+          button[i] = new JButton("");
+          button[i].addActionListener(this);
+          button[i].setBackground(Color.WHITE);
+          add(button[i]);
+      }
+      reset = new JButton("RESET");
+      reset.addActionListener(this);
+      reset.setBackground(Color.WHITE);
+      add(reset);
+      setLayout(new GridLayout(4, 3));
     }
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
         Object source = e.getSource();
         char symbol = 'X';
-        turn = !turn;
-
-        if(turn)
-        {
-            symbol = 'O';
+        if(myTurn){
+          symbol = 'O';
         }
-
+        myTurn = !myTurn;
         for (int i = 0; i < button.length; i++)
         {
-            if(source.equals(reset))
-            {
-                button[i].setText("");
-            }
+          if (source.equals(reset)) 
+          {
+            button[i].setText("");
+            button[i].setEnabled(true);
+            button[i].setBackground(Color.WHITE);
+            System.out.println("WYSZŁO!");
+          }
 
-            if(source.equals(button[i]))
-            {
-                button[i].setText("" + symbol);
-            }
-            
+          if(source.equals(button[i]))
+          {
+            button[i].setText(""+symbol);
+            button[i].setEnabled(false);
+          }
+        }
+        if(checkResult())
+        {
+          for (int i = 0; i < button.length; i++){
+            button[i].setEnabled(false);
+          }
         }
 
-
-
     }
-
-   
-  
-
-    
+    public boolean checkResult()
+    {
+      boolean result = false;
+      for(int fi = 0; fi < 8; fi++)
+      {
+        int i = fi*3;
+        int id[] = new int[3];
+        if(fi < 3)
+        {
+          id[0] = i;
+          id[1] = i+1;
+          id[2] = i+2;
+        }else if(fi < 6)
+        {
+          id[0] = fi - 3;
+          id[1] = fi;
+          id[2] = fi+3;
+        }else if(fi == 6)
+        {
+          id[0] = 0;
+          id[1] = 4;
+          id[2] = 8;
+        }else if(fi == 7)
+        {
+          id[0] = 2;
+          id[1] = 4;
+          id[2] = 6;
+        }
+        if(button[id[0]].getText().equals(button[id[1]].getText()) 
+        && button[id[0]].getText().equals(button[id[2]].getText())
+        && !button[id[0]].getText().equals(""))
+        {
+          button[id[0]].setBackground(Color.GREEN);
+          button[id[1]].setBackground(Color.GREEN);
+          button[id[2]].setBackground(Color.GREEN);
+          result = true;
+        }
+      }
+      return result;
+    }
 }
